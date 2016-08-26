4ae6375eb6d2e2ddfcf599bb95738af0761e5b86
Roby Rodriguez <andrey.roba@gmail.com>
2016-08-26 12:05:39 +0300

    More URL corrections

--- src/js/jQueryPlugin.js
+++ src/js/jQueryPlugin.js
@@ -29,6 +29,6 @@
 	 * @type {{cssFile: string}}
 	 */
 	$.fn.htmlToc.defaults = {
-		cssFile: 'https://cdn.rawgit.com/roby-rodriguez/html-toc/master/dist/css/jquery.html-toc.min.css'
+		cssFile: 'https://rawgit.com/roby-rodriguez/html-toc/master/dist/css/jquery.html-toc.min.css'
 	};
 })(jQuery);
